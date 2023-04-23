5dbf32aaa4c9945bad067c989d553e6bd1e479c9
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-04-23 16:09:36 +0100

    Dynamic date used in footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css"
 import "./components/Utilities.css"
 import Navbar from "./components/Navbar"
 import Page from "./pages/Page"
+import Footer from "./components/Footer"
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Navbar />
 			<Page />
+			<Footer />
 		</BrowserRouter>
 	)
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,47 @@
+import { Link } from "react-router-dom"
+
+const Footer = () => {
+	const date = new Date().getFullYear()
+
+	return (
+		<>
+			<footer className="footer bg-dark py-5">
+				<div className="container grid grid-3">
+					<div>
+						<h1>Loruki</h1>
+						<p>Copyright &copy; {date}</p>
+					</div>
+					<nav>
+						<ul>
+							<li>
+								<Link>Home</Link>
+							</li>
+							<li>
+								<Link>Features</Link>
+							</li>
+							<li>
+								<Link>Docs</Link>
+							</li>
+						</ul>
+					</nav>
+					<div className="social">
+						<Link href="#">
+							<i className="fab fa-github fa-2x"></i>
+						</Link>
+						<Link href="#">
+							<i className="fab fa-facebook fa-2x"></i>
+						</Link>
+						<Link href="#">
+							<i className="fab fa-instagram fa-2x"></i>
+						</Link>
+						<Link href="#">
+							<i className="fab fa-twitter fa-2x"></i>
+						</Link>
+					</div>
+				</div>
+			</footer>
+		</>
+	)
+}
+
+export default Footer
